728bb5bb528f599d978cad84b2b48e99ccb202db
이정민 <102846582+Leejungmin1119@users.noreply.github.com>
2025-10-11 20:52:31 +0900

    today_develop

--- Assets/script/object_folder/Hack.cs
+++ Assets/script/object_folder/Hack.cs
@@ -1,27 +1,25 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 public class Hack : MonoBehaviour
 {
+    public float Player_Hp, Player_Speed, Player_Damage;
+    public bool Iscruse;
+    public int[] Cruse_amount;
+    public static float CurrentAimAngle { get; set; } = 0f;
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        Hack_Manager.instance.Player_Object = GetComponent<Rigidbody2D>();
+        Hack_Manager.instance.Player_Object = this.gameObject;
     }
 
-    // 공격 키 (예: Spacebar)를 눌렀을 때 발사
     void Update()
     {
-        // 1. 공격 키 입력 확인 (예: 스페이스바)
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // 2. 현재 입력된 방향키 값을 가져옵니다.
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-        }
     }
 }
+
 
--- Assets/script/object_folder/Hack_Manager.cs
+++ Assets/script/object_folder/Hack_Manager.cs
@@ -5,13 +5,10 @@ public class Hack_Manager : MonoBehaviour
 
     public static Hack_Manager instance;
 
-    [Header("플레이어 기본 정보")]
-    public float Player_Hp, Player_Speed, Player_Damage;
-    Rigidbody2D Player_Object;
+    [Header("플레이어 시스템")]
+    public GameObject Player_Object;
     public int Player_Level;
-    [Header("저주 상태 변수")]
-    public bool Iscruse;
-    public int[] Cruse_amount;
+
 
 
     // 싱글턴 인스턴스
